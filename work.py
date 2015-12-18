84ec965a73756c5bf5aecedfa60c75389b35f641
zhenning <931208707@qq.com>
2015-12-18 16:08:17 +0800

    'work'

--- work.py
+++ work.py
@@ -1,8 +1,13 @@
-import bs4,requests
+import bs4,requests,re
 def get_messages(im):
 	message = bs4.BeautifulSoup(im.content,'html.parser')
 	mg = message.find(id="item_detail")
 	print mg.text
+	num = mg.find_all('dd')[2].text.encode('utf-8')
+	number = re.match(r'(\d+)(\W?)(\d+)(\W?)(\d+)(\W?)(\d+)(\W?)(\d+)',num).group().split('-')
+	end = ''.join(number)
+	address = requests.get('http://210.35.251.243/opac/ajax_douban.php?isbn=' + end)
+	print eval(address.content)['summary'].decode('unicode-escape')
 def match(name):
 	response = requests.get('http://210.35.251.243/opac/ajax_top_lend_shelf.php')
 	soup = bs4.BeautifulSoup(response.content,'html.parser')
